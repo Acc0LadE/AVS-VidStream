d377e3ec2fcbcf044fe10078c10b744219ca7d47
gk__ <142667881+Acc0LadE@users.noreply.github.com>
2024-02-21 19:00:34 +0530

    Update client.py
    socket connection from client side

--- client.py
+++ client.py
@@ -1,1 +1,10 @@
+#packages
 import socket
+from vidstream import *
+import threading
+import tkinter
+
+#store IPv4 address in variable and establish connection using sockets & ports
+ip_address = socket.gethostbyname(socket.gethostname())
+server = StreamingServer(ip_address,7777)
+reciever = StreamingServer(ip_address,6666)
