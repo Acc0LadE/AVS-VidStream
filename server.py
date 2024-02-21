e3ae019a0c2b5af5bb4ae67128e1d84763bd3256
gk__ <142667881+Acc0LadE@users.noreply.github.com>
2024-02-21 18:51:02 +0530

    Update server.py
    sockets for server

--- server.py
+++ server.py
@@ -1,1 +1,13 @@
+#packages
 import socket
+from vidstream import *
+import threading
+import tkinter
+
+#store IPv4 address in variable and establish connection using sockets & ports
+ip_address = socket.gethostbyname(socket.gethostname())
+server = StreamingServer(ip_address,9999)
+reciever = StreamingServer(ip_address,8888)
+
+
+
