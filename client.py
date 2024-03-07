68286fb8f923439bf55db65385b06af65f819bd3
gk__ <142667881+Acc0LadE@users.noreply.github.com>
2024-03-07 14:54:46 +0530

    trial commit

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import ssl
 import threading
 import tkinter
 
+
 #store IPv4 address in variable and establish connection using sockets & ports
 ip_address = socket.gethostbyname(socket.gethostname())
 server = StreamingServer(ip_address,7777)
